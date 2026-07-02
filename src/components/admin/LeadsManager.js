c663c038f425536f514f1b9773db33379d42a7a7
apple <apple@MacBook-Pro-apple.local>
2026-07-02 18:06:07 +0500

    Fix ReferenceError: useState is not defined in LeadsManager

--- src/components/admin/LeadsManager.js
+++ src/components/admin/LeadsManager.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { 
   Users, Trash2, Upload, Brain, Sparkles, 
   Mail, MessageCircle, Globe, Target, Flame, Snowflake
