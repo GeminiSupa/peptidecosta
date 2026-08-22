b65fe125d492fdf992d6a4b44406e7980411ce28
apple <apple@MacBook-Pro-apple.local>
2026-08-22 18:55:27 +0500

    Add MGID tracking pixel to global layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -105,6 +105,26 @@ export default async function RootLayout({ children }) {
         <Script id="tiktok-pixel" strategy="beforeInteractive">
           {getTikTokPixelBootstrapScript()}
         </Script>
+        {/* Mgid Sensor */}
+        <Script id="mgid-sensor" strategy="afterInteractive">
+          {`(function() {
+              var d = document, w = window;
+              w.MgSensorData = w.MgSensorData || [];
+              w.MgSensorData.push({
+                  cid:988745,
+                  project: "a.mgid.com"
+              });
+              var l = "a.mgid.com";
+              var n = d.getElementsByTagName("script")[0];
+              var s = d.createElement("script");
+              s.type = "text/javascript";
+              s.async = true;
+              var dt = !Date.now?new Date().valueOf():Date.now();
+              s.src = "https://" + l + "/mgsensor.js?d=" + dt;
+              n.parentNode.insertBefore(s, n);
+          })();`}
+        </Script>
+        {/* /Mgid Sensor */}
         <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
